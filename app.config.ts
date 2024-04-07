6d783cba5265b5a7b5428445af93a30e4eefcbd5
moises-herrera <moisesherrera037@gmail.com>
2024-04-07 12:09:14 -0500

    refactor: assets folder

--- app.config.ts
+++ app.config.ts
@@ -6,10 +6,10 @@ const config: ExpoConfig = {
   slug: 'smart-finance-frontend',
   version: '1.0.0',
   orientation: 'portrait',
-  icon: './assets/icon.png',
+  icon: './src/assets/images/icon.png',
   userInterfaceStyle: 'light',
   splash: {
-    image: './assets/splash.png',
+    image: './src/assets/images/splash.png',
     resizeMode: 'contain',
     backgroundColor: '#ffffff',
   },
@@ -19,12 +19,12 @@ const config: ExpoConfig = {
   },
   android: {
     adaptiveIcon: {
-      foregroundImage: './assets/adaptive-icon.png',
+      foregroundImage: './src/assets/images/adaptive-icon.png',
       backgroundColor: '#ffffff',
     },
   },
   web: {
-    favicon: './assets/favicon.png',
+    favicon: './src/assets/images/favicon.png',
   },
 };
 
