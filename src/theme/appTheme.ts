f166bdfd6488fa95d3d7a012656391118a17981c
moises-herrera <moisesherrera037@gmail.com>
2024-04-07 12:09:41 -0500

    style: add app theme colors

---
+++ src/theme/appTheme.ts
@@ -0,0 +1,10 @@
+export const appTheme = {
+  colors: {
+    primary: '#06C35D',
+    secondary: '#E6E5E7',
+    white: '#FFFFFF',
+    black: '#000000',
+    gray: '#E6E5E7',
+    blue: '#469FEB',
+  },
+};
