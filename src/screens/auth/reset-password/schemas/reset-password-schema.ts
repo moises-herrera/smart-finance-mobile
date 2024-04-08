ebd0b0b8e834e10b56725718f23f70778b90234e
moises-herrera <moisesherrera037@gmail.com>
2024-04-07 23:36:20 -0500

    chore: add reset password schema

---
+++ src/screens/auth/reset-password/schemas/reset-password-schema.ts
@@ -0,0 +1,32 @@
+import { z } from 'zod';
+import { passwordPattern } from '../../../../helpers';
+
+/**
+ * Reset password form validation schema.
+ */
+export const ResetPasswordSchema = z
+  .object({
+    password: z
+      .string()
+      .min(8, 'La contraseña debe tener mínimo 8 carácteres')
+      .max(20, 'La contraseña debe tener máximo 20 carácteres')
+      .regex(
+        passwordPattern,
+        'La contraseña debe tener al menos una mayúscula, una minúscula y un número'
+      ),
+    confirmPassword: z.string().min(1, 'La confirmación es requerida'),
+  })
+  .superRefine(({ password, confirmPassword }, context) => {
+    if (password !== confirmPassword) {
+      context.addIssue({
+        code: z.ZodIssueCode.custom,
+        path: ['confirmPassword'],
+        message: 'Las contraseñas no coinciden',
+      });
+    }
+  });
+
+/**
+ * Reset password form validation schema type.
+ */
+export type ResetPasswordSchemaType = z.infer<typeof ResetPasswordSchema>;
