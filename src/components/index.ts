2106f3e75e44b1366b845c91049a92c0bb96822d
moises-herrera <moisesherrera037@gmail.com>
2024-04-07 22:34:10 -0500

    chore: add mock data

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export * from './input';
 export * from './form-control';
 export * from './button';
 export * from './divider';
+export * from './select';
---
+++ src/mock/countries.ts
@@ -0,0 +1,16 @@
+import { SelectOption } from '../interfaces';
+
+export const countries: SelectOption[] = [
+  { label: 'Argentina', value: 'AR' },
+  { label: 'Bolivia', value: 'BO' },
+  { label: 'Brasil', value: 'BR' },
+  { label: 'Chile', value: 'CL' },
+  { label: 'Colombia', value: 'CO' },
+  { label: 'Ecuador', value: 'EC' },
+  { label: 'Guyana', value: 'GY' },
+  { label: 'Paraguay', value: 'PY' },
+  { label: 'Perú', value: 'PE' },
+  { label: 'Surinam', value: 'SR' },
+  { label: 'Uruguay', value: 'UY' },
+  { label: 'Venezuela', value: 'VE' },
+];
