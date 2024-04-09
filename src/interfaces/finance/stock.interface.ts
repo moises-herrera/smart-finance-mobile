60e01eede88c64b55fa4a014aa075d326d8bf389
moises-herrera <moisesherrera037@gmail.com>
2024-04-08 19:58:06 -0500

    chore: add stock interface

---
+++ src/interfaces/finance/stock.interface.ts
@@ -0,0 +1,13 @@
+/**
+ * Represents the information of a stock.
+ */
+export interface Stock {
+  /** Stock's label. */
+  label: string;
+
+  /** Stock's symbol. */
+  symbol: string;
+
+  /** Stock's price. */
+  price: number;
+}
