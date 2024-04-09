da3f6d3b1526b1f77135865b0f2cbb327079c476
moises-herrera <moisesherrera037@gmail.com>
2024-04-08 19:56:11 -0500

    style: add font light

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ SplashScreen.preventAutoHideAsync();
 
 export default function App() {
   const [fontsLoaded, fontError] = useFonts({
+    'Inter-Light': require('./assets/fonts/inter/Inter-Light.ttf'),
     'Inter-Regular': require('./assets/fonts/inter/Inter-Regular.ttf'),
     'Inter-Medium': require('./assets/fonts/inter/Inter-Medium.ttf'),
     'Inter-SemiBold': require('./assets/fonts/inter/Inter-SemiBold.ttf'),
