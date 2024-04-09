344fb11cabbf492a33763a4a72507a778a44fd98
moises-herrera <moisesherrera037@gmail.com>
2024-04-08 19:58:15 -0500

    chore: add mock stocks

---
+++ src/mock/stocks.ts
@@ -0,0 +1,24 @@
+import { Stock } from 'src/interfaces';
+
+export const stocks: Stock[] = [
+  {
+    label: 'Apple Inc.',
+    symbol: 'AAPL',
+    price: 146.92,
+  },
+  {
+    label: 'Microsoft Corporation',
+    symbol: 'MSFT',
+    price: 277.01,
+  },
+  {
+    label: 'Amazon.com Inc.',
+    symbol: 'AMZN',
+    price: 3372.01,
+  },
+  {
+    label: 'Meta Platforms Inc.',
+    symbol: 'META',
+    price: 332.88,
+  },
+];
