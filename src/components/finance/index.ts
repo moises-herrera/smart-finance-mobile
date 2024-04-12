919d5621db5409a320e387893d4b2d9b74525abf
LaFLSMDFR <miguelhh2423@gmail.com>
2024-04-12 10:32:01 -0500

    fix: Resolucion de conflictos

--- src/components/finance/index.ts
+++ src/components/finance/index.ts
@@ -1,8 +1,5 @@
 export * from './balance-card';
 export * from './operations-list';
-export * from './stock-list-item';
-<<<<<<< HEAD
-export * from './stocks-list';
-=======
 export * from './stock-dialog';
->>>>>>> ac76e3342e725bf906e309a03a1dbdd9d6eebf3a
+export * from './stock-list-item';
+export * from './stocks-list';
