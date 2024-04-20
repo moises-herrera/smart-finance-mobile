e6dd5e3089776bfe7f76a9f7558de182efe67c96
moises-herrera <moisesherrera037@gmail.com>
2024-04-20 18:50:49 -0500

    feat: validate access token

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -1,18 +1,36 @@
 import { createStackNavigator } from '@react-navigation/stack';
 import { AuthNavigator, ProtectedNavigator } from 'src/navigation/navigators';
 import { AppStackParamList } from 'src/interfaces';
+import { useAppDispatch, useAppSelector } from 'src/hooks';
+import { Loading } from 'src/components';
+import { useEffect } from 'react';
+import { validateAccessToken } from 'src/redux/auth';
 
 const Stack = createStackNavigator<AppStackParamList>();
 
 export const AppNavigator = () => {
+  const dispatch = useAppDispatch();
+  const status = useAppSelector(({ auth }) => auth.status);
+
+  useEffect(() => {
+    dispatch(validateAccessToken());
+  }, []);
+
+  if (status === 'loading') {
+    return <Loading />;
+  }
+
   return (
     <Stack.Navigator
       screenOptions={{
         headerShown: false,
       }}
     >
-      <Stack.Screen name="Auth" component={AuthNavigator} />
-      <Stack.Screen name="Home" component={ProtectedNavigator} />
+      {status === 'authenticated' ? (
+        <Stack.Screen name="Home" component={ProtectedNavigator} />
+      ) : (
+        <Stack.Screen name="Auth" component={AuthNavigator} />
+      )}
     </Stack.Navigator>
   );
 };
