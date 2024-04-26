5af6e2e1930274f75421d308e1c704eb212f3efa
moises-herrera <moisesherrera037@gmail.com>
2024-04-26 18:01:45 -0500

    feat: render user data in dashboard

--- src/screens/home/dashboard/Dashboard.tsx
+++ src/screens/home/dashboard/Dashboard.tsx
@@ -2,11 +2,15 @@ import { View } from 'react-native';
 import { styles } from './styles';
 import { BalanceCard, StocksList } from 'src/components';
 import { stocks } from 'src/mock';
+import { useAppSelector } from 'src/hooks';
 
 export const Dashboard = () => {
+  const userFullName = useAppSelector(({ auth: { user } }) => user?.fullName);
+  const balance = useAppSelector(({ auth: { user } }) => user?.balance || 0);
+
   return (
     <View style={styles.container}>
-      <BalanceCard balance={10000} name="John Doe" />
+      <BalanceCard balance={balance} name={userFullName as string} />
       <StocksList title="Acciones destacadas" stocks={stocks.slice(0, 5)} />
     </View>
   );
