9e0707a597796b73bb1db134956728fb1ed4ef46
moises-herrera <moisesherrera037@gmail.com>
2024-04-26 19:27:33 -0500

    chore: add parse acquired stocks helper

---
+++ src/helpers/parse-acquired-stocks.ts
@@ -0,0 +1,16 @@
+import { AcquiredStock, Stock } from 'src/interfaces';
+
+/**
+ * Parse the acquired stocks to a list of stocks.
+ *
+ * @param acquiredStocks The acquired stocks to parse.
+ * @returns The list of stocks.
+ */
+export const parseAcquiredStocks = (
+  acquiredStocks: AcquiredStock[]
+): Stock[] => {
+  return acquiredStocks.map(({ stock, totalQuantity }) => ({
+    ...stock,
+    price: totalQuantity,
+  }));
+};
