76836b867234c9770b565f9804452bd4fbeb3178
moises-herrera <moisesherrera037@gmail.com>
2024-04-26 19:06:34 -0500

    chore: add acquired stock state

--- src/redux/interfaces/index.ts
+++ src/redux/interfaces/index.ts
@@ -2,3 +2,4 @@ export * from './types';
 export * from './auth-state.interface';
 export * from './otp-state.interface';
 export * from './operation-state.interface';
+export * from './acquired-stock-state.interface';
