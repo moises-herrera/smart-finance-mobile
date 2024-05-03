8f6faa9b562d130f9f27431adebc7dc5c678bd58
moises-herrera <moisesherrera037@gmail.com>
2024-05-03 18:24:47 -0500

    feat: add thunk in stock slice

--- src/redux/stock/index.ts
+++ src/redux/stock/index.ts
@@ -1,1 +1,2 @@
 export * from './stockSlice';
+export * from './stockThunks';
--- src/redux/stock/stockSlice.ts
+++ src/redux/stock/stockSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import { StockState } from 'src/redux/interfaces';
+import { getAvailableStocks } from './stockThunks';
 
 const initialState: StockState = {
   stocks: [],
@@ -10,8 +11,26 @@ const initialState: StockState = {
 export const stockSlice = createSlice({
   name: 'stock',
   initialState,
-  reducers: {},
+  reducers: {
+    clearErrorMessage: (state) => {
+      state.stockErrorMessage = null;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(getAvailableStocks.pending, (state) => {
+      state.areLoadingStocks = true;
+      state.stockErrorMessage = null;
+    });
+    builder.addCase(getAvailableStocks.fulfilled, (state, action) => {
+      state.areLoadingStocks = false;
+      state.stocks = action.payload;
+    });
+    builder.addCase(getAvailableStocks.rejected, (state, action) => {
+      state.areLoadingStocks = false;
+      state.stockErrorMessage = action.payload?.message;
+    });
+  },
 });
 
 // Action creators are generated for each case reducer function
-export const {} = stockSlice.actions;
+export const { clearErrorMessage } = stockSlice.actions;
