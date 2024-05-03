ad7dd623bae89e29bceb3652d4987e363232f3c2
moises-herrera <moisesherrera037@gmail.com>
2024-05-03 18:09:02 -0500

    refactor: add useGetCountries custom hook

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,3 +1,4 @@
 export * from './useForm';
 export * from './useAppDispatch';
 export * from './useAppSelector';
+export * from './useGetCountries';
---
+++ src/hooks/useGetCountries.ts
@@ -0,0 +1,60 @@
+import { useEffect, useMemo, useState } from 'react';
+import { SelectOption } from 'src/interfaces';
+import { useAppSelector } from './useAppSelector';
+import { useAppDispatch } from './useAppDispatch';
+import { getCountries } from 'src/redux/country';
+import { displayToast } from 'src/redux/ui';
+
+export const useGetCountries = (initialCountryIdSelected?: string) => {
+  const dispatch = useAppDispatch();
+  const countries = useAppSelector(({ country: { countries } }) => countries);
+  const areLoadingCountries = useAppSelector(
+    ({ country: { areLoadingCountries } }) => areLoadingCountries
+  );
+  const countriesErrorMessage = useAppSelector(
+    ({ country: { countriesErrorMessage } }) => countriesErrorMessage
+  );
+  const [countryIdSelected, setCountryIdSelected] = useState<string>(
+    initialCountryIdSelected ?? ''
+  );
+
+  useEffect(() => {
+    if (!countries.length) {
+      dispatch(getCountries());
+    }
+  }, [countries, dispatch]);
+
+  useEffect(() => {
+    if (countriesErrorMessage) {
+      dispatch(displayToast({ message: countriesErrorMessage, type: 'error' }));
+    }
+  }, [countriesErrorMessage]);
+
+  const countriesOptions = useMemo<SelectOption[]>(
+    () =>
+      countries.map<SelectOption>((country) => ({
+        label: country.name,
+        value: country._id,
+      })),
+    [countries]
+  );
+  const currenciesOptions = useMemo<SelectOption[]>(() => {
+    if (!countryIdSelected) return [];
+
+    const currencies = countries.find(({ _id }) => _id === countryIdSelected);
+    const options =
+      currencies?.currencies.map<SelectOption>((currency) => ({
+        label: currency.name,
+        value: currency._id,
+      })) || [];
+    return options;
+  }, [countryIdSelected, countries]);
+
+  return {
+    countries,
+    areLoadingCountries,
+    countriesOptions,
+    currenciesOptions,
+    setCountryIdSelected,
+  };
+};
