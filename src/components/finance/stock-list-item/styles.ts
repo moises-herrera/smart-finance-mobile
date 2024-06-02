fc186cf80d76950a7eb59a2e2ca677e09d8c2b90
moises-herrera <moisesherrera037@gmail.com>
2024-06-01 23:15:40 -0500

    feat: add stock icon

--- src/components/finance/stock-list-item/styles.ts
+++ src/components/finance/stock-list-item/styles.ts
@@ -4,7 +4,16 @@ export const styles = StyleSheet.create({
   container: {
     width: '100%',
     padding: 10,
-    flexDirection: 'row',
+    flexDirection: 'column',
     justifyContent: 'space-between',
+  },
+  iconContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: 50,
+    height: 50,
+    borderRadius: 100,
+    overflow: 'hidden',
+    marginRight: 10,
   },
 });
