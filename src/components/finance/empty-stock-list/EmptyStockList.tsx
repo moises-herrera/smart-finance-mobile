e43ab6369de76351457c4255dadf14d476c798d3
moises-herrera <moisesherrera037@gmail.com>
2024-06-02 11:00:32 -0500

    chore: add empty stock list component

---
+++ src/components/finance/empty-stock-list/EmptyStockList.tsx
@@ -0,0 +1,57 @@
+import { FC } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import { Text, View } from 'react-native';
+import { Button } from 'src/components/ui';
+import { globalStyles } from 'src/styles';
+import { appTheme } from 'src/theme';
+import { styles } from './styles';
+
+interface EmptyStockListProps {
+  message: string;
+  buttonLabel: string;
+}
+
+export const EmptyStockList: FC<EmptyStockListProps> = ({
+  message,
+  buttonLabel,
+}) => {
+  const navigation = useNavigation();
+
+  const redirectToMarket = (): void => {
+    navigation.navigate('Home', {
+      screen: 'Market',
+    });
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text
+        style={[
+          globalStyles.textBase,
+          {
+            textAlign: 'center',
+            fontSize: 14,
+          },
+        ]}
+      >
+        {message}
+      </Text>
+
+      <Button
+        style={{
+          button: {
+            backgroundColor: appTheme.colors.white,
+            borderWidth: 0.5,
+            borderRadius: 6,
+            borderColor: appTheme.colors.blue,
+          },
+          buttonText: {
+            color: appTheme.colors.blue,
+          },
+        }}
+        label={buttonLabel}
+        onPress={redirectToMarket}
+      />
+    </View>
+  );
+};
